c975b97388a7371bbf929d10a6d940b27069d3d6
Damiano Clementel <damiano.clementel@gmail.com>
2020-06-17 18:49:28 +0200

    Added automatic trimming, plots and cyclic pfbuild control

--- script/pipeline.py
+++ script/pipeline.py
@@ -1,12 +1,17 @@
 # Common dependencies
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
 import subprocess
 import argparse
-import tempfile
 import shutil
 import glob
 import sys
 import os
 import re
+
+# Set plotting non iteractive
+matplotlib.use('Agg')
 
 # Updating python path
 sys.path.append(os.path.dirname(os.path.realpath(__file__) + '/..'))
@@ -14,10 +19,10 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__) + '/..'))
 # Custom dependencies
 from src.msa import MSA
 from src.bjob import Bjob
-# from ..src.transform import Compose
-# from ..src.transform import OccupancyTrim
-# from ..src.transform import OccupancyFilter
-# from ..src.transform import MakeNonRedundant
+from src.transform import Compose
+from src.transform import OccupancyTrim
+from src.transform import OccupancyFilter
+from src.transform import MakeNonRedundant
 
 
 # Read arguments
@@ -131,6 +136,7 @@ for i in range(0, num_clusters, batch_size):
         out = subprocess.run(
             capture_output=True,  # Capture console output
             encoding='utf-8',  # Set output encoding
+            env=env,  # Set custom environment
             cwd=cluster_path,  # Set directory
             args=['pfbuild', '-withpfmake', '-db', 'uniprot']
         )
@@ -148,6 +154,7 @@ for i in range(0, num_clusters, batch_size):
     out = subprocess.run(
         capture_output=True,
         encoding='utf-8',
+        env=env,
         cwd=batch_path,
         args=['check_uniprot.pl']
     )
@@ -168,8 +175,9 @@ for i in range(0, num_clusters, batch_size):
         shutil.move(cluster_path, build_path + '/' + cluster_name)
         # Debug
         print('Moved {} to {}'.format(cluster_path, build_path))
+
     # Debug
-    print('There are {:d} ({:.03f}%) possible kept clusters: {}'.format(
+    print('There are {:d} ({:.03f}%) remaining clusters: {}'.format(
         # Number of possible MGnifam
         len(kept_clusters),
         # Rate of possible MGnifam clusters
@@ -195,3 +203,203 @@ for i in range(0, num_clusters, batch_size):
         # List all possible Pfam
         ', '.join(os.path.basename(path) for path in uni_clusters)
     ))
+
+    # Delete current batch folder (free space)
+    shutil.rmtree(batch_path)
+
+# Define a common multiple sequence alignment transformation pipeline
+transform = Compose([
+    # Exclude regions outside N- and C- terminal
+    OccupancyTrim(
+        # Use custom threshold:
+        # threshold = lambda x: np.mean(x) - np.std(x),
+        threshold=0.5,
+        # Set threshold inclusive
+        inclusive=True
+    ),
+    # Exclude sequences with less than half occupancy
+    OccupancyFilter(
+        # Use custom threshold:
+        threshold=0.5,
+        # Set threshold inclusive
+        inclusive=True
+    ),
+    # Make non redundant
+    MakeNonRedundant(
+        # Define redundancy threshold
+        threshold=0.8,
+        # Set threshold exclusive
+        inclusive=False
+    )
+])
+# Initialize summary dictionary
+summary = dict()
+# Get list of clusters paths
+cluster_paths = glob.glob(build_path + '/MGYP*')
+# Debug
+print('There are {:d} clusters in build: {}'.format(
+    len(cluster_paths)
+))
+# For every file in build, run pfbuild again
+for cluster_path in cluster_paths:
+    # Get cluster name
+    cluster_name = os.path.basename(cluster_path)
+    cluster_name = os.path.dirname(cluster_name)
+    # Initialize summary entry for current cluster
+    summary[cluster_name] = {
+        # Prettiness score
+        'pre_prettiness': None,  # Before trimming
+        'post_prettiness': None,  # After trimming
+        # Occupancy distribution
+        'pre_occupancy': None,  # Before trimming
+        'post_occupancy': None,  # After occupancy
+        # Conservation distribution
+        'pre_conservation': None,  # Before trimming
+        'post_conservation': None  # After occupancy
+    }
+    # Load multiple seed alignment
+    seed = MSA().from_aln(cluster_path)
+    # Save summary before trimming
+    summary[cluster_name] = {**summary[cluster_name], **{
+        'pre_prettiness': MSA.prettiness(seed.aln),
+        'pre_occupancy': MSA.occupancy(seed.aln)[0],
+        'pre_conservation': MSA.conservation(seed.aln)[0]
+    }}
+    # Execute trim, substitute original seed
+    seed = transform(seed)
+    # Save summary after trimming
+    summary[cluster_name] = {**summary[cluster_name], **{
+        'post_prettiness': MSA.prettiness(seed.aln),
+        'post_occupancy': MSA.occupancy(seed.aln)[0],
+        'post_conservation': MSA.conservation(seed.aln)[0]
+    }}
+    # Store new SEED alignment
+    seed.to_aln(cluster_path)
+
+# Plot summary
+fig, axs = plt.subplots(2, 3, figsize=(30, 20), sharex='col', sharey='col')
+# Set titles
+_ = axs[0, 0].set_title('Pre-trim prettiness')
+_ = axs[0, 1].set_title('Pre-trim occupancy')
+_ = axs[0, 2].set_title('Pre-trim conservation')
+_ = axs[1, 0].set_title('Post-trim prettiness')
+_ = axs[1, 1].set_title('Post-trim occupancy')
+_ = axs[1, 2].set_title('Post-trim conservation')
+# Plot prettiness (pre-trim)
+pre_prettiness = [summary[k]['pre_prettiness'] for k in summary.keys()]
+_ = axs[0, 0].hist(pre_prettiness, bins=100)
+_ = axs[0, 0].axvline(np.mean(pre_prettiness))
+# Plot prettiness (post-trim)
+post_prettiness = [summary[k]['post_prettiness'] for k in summary.keys()]
+_ = axs[1, 0].hist(post_prettiness, bins=100)
+_ = axs[1, 0].axvline(np.mean(post_prettiness))
+# Plot occupancy distribution (pre-trim)
+for k in summary.keys():
+    _ = axs[0, 1].hist(
+        summary[k]['pre_occupancy'],
+        density=True,
+        stacked=True,
+        bisn=100
+    )
+# Plot occupancy distribution (post-trim)
+for k in summary.keys():
+    _ = axs[1, 1].hist(
+        summary[k]['post_occupancy'],
+        density=True,
+        stacked=True,
+        bins=100
+    )
+# Plot conservation distribution (pre-trim)
+for k in summary.keys():
+    _ = axs[0, 2].hist(
+        summary[k]['pre_conservation'],
+        density=True,
+        stacked=True,
+        bins=100
+    )
+# Plot conservation distribution (post-trim)
+for k in summary.keys():
+    _ = axs[1, 2].hist(
+        summary[k]['post_conservation'],
+        density=True,
+        stacked=True,
+        bins=100
+    )
+# Save figure to file
+_ = plt.savefig(build_path + '/trim.png')
+# Close plot
+_ = plt.close()
+# Delete summary variable (free some space)
+del summary
+
+# Define set of running jobs
+bjobs = list()
+# Debug
+print('Clusters for pfbuild:\n{}'.format('\n'.join(cluster_paths)))
+# Run pfbuild for every cluster in batch
+for cluster_path in cluster_paths:
+    # Run pfbuild current cluster directory
+    out = subprocess.run(
+        capture_output=True,  # Capture console output
+        encoding='utf-8',  # Set output encoding
+        env=env,  # Set custom environment
+        cwd=cluster_path,  # Set directory
+        args=['pfbuild', '-withpfmake', '-db', 'uniprot']
+    )
+    # Debug
+    print('pfbuild:', out)
+    # Get process id as string
+    job_id = Bjob.id_from_string(out.stdout)
+    # Save job id
+    bjobs.append(Bjob(id=job_id, status='RUN'))
+
+# Check running pfbuild jobs
+Bjob.check(bjobs, delay=30)
+
+# Run check_uniprot.pl in current build directory
+out = subprocess.run(
+    capture_output=True,
+    encoding='utf-8',
+    env=env,
+    cwd=build_path,
+    args=['check_uniprot.pl']
+)
+# Debug
+print('check_uniprot.pl:', out)
+
+# Define kept clusters (MGnifam)
+kept_clusters = glob.glob(build_path + '/MGYP*')
+# Define discarded clusters
+bias_clusters = glob.glob(build_path + '/BIAS/MGYP*')
+uni_clusters = glob.glob(build_path + '/Uniprot/MGYP*')
+# Debug
+print('There are {:d} ({:.03f}%) remaining clusters: {}'.format(
+    # Number of possible MGnifam
+    len(kept_clusters),
+    # Rate of possible MGnifam clusters
+    len(kept_clusters) / (len(bias_clusters) + len(uni_clusters) + len(kept_clusters)),
+    # List all possible MGnifam
+    ', '.join(os.path.basename(path) for path in kept_clusters)
+))
+# Debug
+print('There are {:d} ({:.03f}%) clusters in BIAS: {}'.format(
+    # Number of possible Pfam
+    len(bias_clusters),
+    # Rate of possible Pfam clusters
+    len(bias_clusters) / (len(bias_clusters) + len(uni_clusters) + len(kept_clusters)),
+    # List all possible Pfam
+    ', '.join(os.path.basename(path) for path in bias_clusters)
+))
+# Debug
+print('There are {:d} ({:.03f}%) clusters in Uniprot: {}'.format(
+    # Number of possible Pfam
+    len(uni_clusters),
+    # Rate of possible Pfam clusters
+    len(uni_clusters) / (len(bias_clusters) + len(uni_clusters) + len(kept_clusters)),
+    # List all possible Pfam
+    ', '.join(os.path.basename(path) for path in uni_clusters)
+))
+# Remove clusters in BIAS
+shutil.rmtree(build_path + '/BIAS')
+# Remove clusters in Uniprot
+shutil.rmtree(build_path + '/Uniprot')
