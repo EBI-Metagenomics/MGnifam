70404aa6fe62430530885c7cdbe27fd10a978ab3
Damiano Clementel <damiano.clementel@gmail.com>
2020-06-23 14:16:37 +0200

    Updated seed creation pipeline

--- src/seed.py
+++ src/seed.py
@@ -4,8 +4,10 @@ This script substitutes mgseed and create_seed_from_cluster.pl
 """
 
 # Dependencies
+import numpy as np
 import subprocess
 import tempfile
+import time
 import gzip
 import re
 import os
@@ -14,28 +16,18 @@ import os
 class Seed(object):
 
     # Constructor
-    def __init__(self, clusters_path, mgnify_path, out_path, env=os.environ.copy()):
+    def __init__(self, clusters_path, mgnify_path, env=os.environ.copy()):
         # Path to clusters .tsv[.gz] file
         self.clusters_path = clusters_path
         # Path to sequences .fa[.gz] file
         self.mgnify_path = mgnify_path
-        # Path to output directory
-        self.out_path = out_path
         # Environmental variables used in external scritps execution
         self.env = env
 
-    # Wrapper for main
-    def __call__(self, *args, **kwargs):
-        # Execute main
-        return self.main(*args, **kwargs)
-
     # Execute pipeline
-    def main(self, cluster_name):
+    def main(self, cluster_name, out_path):
 
-        # Initialize cluster sequences dict(accession: fasta)
-        cluster_sequences = dict()
-
-        # Get cluster members (squence accession numbers)
+        # Get cluster members (list of squence accession numbers)
         cluster_members = self.get_cluster_members(cluster_name)
         # Case there is no cluster member available
         if not cluster_members:
@@ -47,6 +39,11 @@ class Seed(object):
 
         # Get fasta sequences
         cluster_sequences = self.get_sequences_fasta(cluster_members)
+        # Loop throuth each cluster (sequence accession, fasta) pair
+        for acc, fasta in cluster_sequences.items():
+            # Case fasta entry is none
+            if fasta is None:
+                del cluster_sequences[acc]  # Delete entry
         # Case number of retrieved sequences does not match cluster members
         if len(cluster_members) != len(cluster_sequences):
             # Raise new exception
@@ -55,13 +52,27 @@ class Seed(object):
                 self.mgnify_path  # Fasta sequences path
             ))
 
-        # TODO Compute compositional bias (MobiDB Lite)
-        # TODO Discard clusters with high low complexity/disorder regions
+        # Compute compositional bias (MobiDB Lite)
+        comp_bias = self.compute_comp_bias(cluster_sequences)
+        # TODO Discard cluster with many low complexity/disorder residues
 
         # TODO Compute multiple sequence alignment
 
+
     # Get cluster members
     def get_cluster_members(self, cluster_name):
+        """Retrieve sequences accession numbers
+        Takes cluster name as input and searches in given clusters file the
+        sequences associated with that file
+
+        Args
+        cluster_name (str)  Name of the cluster whose associate sequences
+                            accession numbers must be searched
+
+        Return
+        (list)              List of sequences accession numbers found to be
+                            associated with given cluster name
+        """
 
         # Check if clusters file is compressed
         is_gzip = bool(re.search(r'\.gz$', self.clusters_path))
@@ -73,7 +84,7 @@ class Seed(object):
             file_handler = lambda path: gzip.open(path, 'rt')
 
         # Initialize a list of sequence accession
-        cluster_members = list()
+        sequences_acc = list()
         # Open file with defined file handler
         with file_handler(self.clusters_path) as clusters_file:
             # Loop through every line in file
@@ -90,12 +101,24 @@ class Seed(object):
                 if found_cluster_name != cluster_name:
                     continue  # Skip iteration
                 # Otherwise, store found sequence accesssion
-                cluster_members.append(found_sequence_acc)
-        # Return list of cluster members
-        return cluster_members
+                sequences_acc.append(found_sequence_acc)
+        # Return list of founs sequences accession numbers
+        return sequences_acc
 
     # Get sequences fasta
-    def get_sequences_fasta(self, cluster_members):
+    def get_sequences_fasta(self, sequences_acc):
+        """Retrieve sequences residues
+        Takes a list of sequences accessions and search for the associated
+        entry by scanning given fasta file headers.
+
+        Args
+        sequences_acc (list)    List of sequences accession numbers whose
+                                residues must be found in given fasta file
+
+        Return
+        (dict(str: str))        Dictionary containing sequences accession
+                                numbers as keys and fasta entries as values
+        """
 
         # Check if sequences fasta file is compressed
         is_gzip = bool(re.search(r'\.gz$', self.clusters_path))
@@ -106,61 +129,79 @@ class Seed(object):
             # Define a compressed file handler
             file_handler = lambda path: gzip.open(path, 'rt')
 
-        # Cast list of cluster members to set
-        cluster_members = set(cluster_members)
-
-        # Initialize a list of sequence accession
-        cluster_sequences = {'': ''}
+        # Initialize output dictionary with sequences accession numbers as keys
+        sequences = {acc: None for acc in sequences_acc}
+        # Initialize current sequence accession
+        curr_acc = None
         # Open file with defined file handler
         with file_handler(self.clusters_path) as clusters_file:
             # Loop through every line in file
             for line in clusters_file:
                 # Check if line matches fasta header
-                match_header = re.search(r'^>(.*)[\n\r]*$', line)
-                # Define previous header
-                curr_header = [*sequences.keys()][-1]
-                # Case current line is header
-                if match_header:
-                    # Define key (remove special characters)
-                    header = match_header.group(1)
-                    # Get sequence accession
-                    curr_acc = re.search(r'^([a-zA-Z0-9]+)[ \t]+', curr_header)
-                    # Delete previous header if not cluster member
-                    if curr_acc not in cluster_members:
-                        # Delete previous header
-                        del sequences[curr_header]
-                    # Update current header
-                    curr_header = header
-                    # Define new entry in sequence dictionary
-                    sequences[curr_header] = ''
+                match_header = re.search(r'^>([a-zA-Z0-9]+).*[\n\r]*$', line)
                 # Case current line is not header
+                if not match_header:
+                    # Check if there is a valid accession number set
+                    if curr_acc is not None:
+                        # Store residues in current line
+                        sequences[curr_acc] += re.sub(r'[^a-zA-Z]+', '', line)
+                    # Otherwise, skip iteration
+                # Case current line is header
                 else:
-                    # Append current sequence to its header
-                    sequences[curr_header] += re.sub(r'[^a-zA-Z]+', '', line)
-            # Get last sequence header
-            curr_header = [*cluster_sequences.keys()][-1]
-            # Get last sequence accession
-            curr_acc = re.search(r'^([a-zA-Z0-9]+)[ \t]+', curr_header)
-            # Delete last sequence if not cluster member
-            del cluster_sequences[curr_header]
-        # Return retrieved sequences
-        return cluster_sequences
+                    # Set sequence accession
+                    curr_acc = match_header.group(1)
+                    # Case current accession is not one of the searched one
+                    if curr_acc not in set(sequences.keys()):
+                        # Discard sequence accession
+                        curr_acc = None
+                    # Case current accession must be stored
+                    else:
+                        # Retrieve header line without special characters
+                        header = re.sub(r'[\n\r]*$', '', line)
+                        # Store header line (followed by newline)
+                        sequences[curr_acc] = header + '\n'
+        # Return filled sequences dictionary
+        return sequences
 
     # Get compositional bias for cluster members
-    def get_compositional_bias(self, sequences):
+    def compute_comp_bias(self, sequences):
+        """Compute compositional bias
+        Given some sequences fasta entries, compute compositional bias over
+        them by running MobiDB Lite classifier.abs($0)
+
+        Args
+        sequences (dict(str: str))  Dictionary containing sequence identifier
+                                    (e.g. sequence accession number) as key
+                                    and associated fasta entry as value
+
+        Return
+        (float)                     Compositional bias for given sequences,
+                                    i.e. the number of disordered residues
+                                    over the total number of residues
+        """
+
         # Define temporary input file
         fasta_file = tempfile.NamedTemporaryFile(delete=False)
         # Define temporary output file
         out_file = tempfile.NamedTemporaryFile(delete=False)
 
-        # Add header entry
+        # Make input fasta file
         with open(fasta_file.name, 'w') as ff:
             # Fill FASTA input file
-            for header in sequences.keys():
-                # Write header row
-                ff.write('>' + header + '\n')
-                # Write content row
-                ff.write(sequences[header] + '\n')
+            for sequence_acc in sequences.keys():
+                # Write fasta entry
+                ff.write(sequences[sequence_acc])
+
+        # Initialize residues dict(sequence acc: np.array(residues))
+        residues = {}
+        # Fill rediues dictionary
+        for sequence_acc in sequences.keys():
+            # Split fasta entry in header and residues
+            h, r = tuple(sequences[sequence_acc].split('\n'))
+            # Turn residues string into numpy array
+            r = np.array(list(r))
+            # Store residues for current accession
+            residues[sequence_acc] = r
 
         # Run MobiDB Lite
         _ = subprocess.run(
@@ -173,24 +214,105 @@ class Seed(object):
                 'python3', 'mobidb_lite.py', '-o', out_file, fasta_file.name
             ]
         )
+        # Delete input file
+        os.remove(fasta_file)
 
+        # Initialize disorder dict(sequence acc: list((disorder region)))
+        # I.e. value is a list of tuples(disordered region begin, end)
+        disorder = {acc: list() for acc in sequences.keys()}
         # Get output of MobiDB Lite
         with open(out_file.name, 'r') as of:
             # Loop through every file line
             for line in of:
                 # Match output file line format
-                match = re.search(r'^([a-zA-Z0-9]+)[ \t]+(\d+)[ \t]+(\d+)', line)
+                match = re.search(r'^(\S+)\s+(\d+)\s+(\d+)', line)
                 # Case the format does not match
                 if not match:
                     continue  # Skip iteration
                 # Get sequence accession
                 sequence_acc = match.group(1)
                 # Get starting/ending residues of disordered region
-                disorder_beg, disorder_end = int(match.group(2)), int(match.group(3))
-                # Compute number of disordered residues
-                num_disorder = disorder_end - disorder_beg
-
-        # Delete input file
-        os.remove(fasta_file)
+                disorder_beg = int(match.group(2))
+                disorder_end = int(match.group(3))
+                # Initialize sequence accession entry
+                disorder.setdefault(sequence_acc, list())
+                # Store disordered region begin and end residues
+                disorder[sequence_acc] = (disorder_beg, disorder_end)
         # Delete output file
         os.remove(out_file)
+
+        # Initialize number of residues and number of disordered ones
+        num_residues = 0
+        num_disorder = 0
+        # Cast (region begin, end) to np array of shape (#regions, #residues)
+        for sequence_acc in disorder.keys():
+            # Get number of disordered regions for current accession (rows)
+            n = len(disorder[sequence_acc])
+            # Get number of residues for current accession (columns)
+            m = len(residues[sequence_acc])
+            # Generate a zeroes matrix with given shape
+            x = np.zeros((n, m), dtype=np.uint8)
+            # Set cells in disordered regions to 1
+            for i, (beg, end) in enumerate(disorder[sequence_acc]):
+                # Note that MobiDB Lite regions begin from 1 and are inclusive
+                # Instead, numpy starts from 0 and is exclusive wrt end
+                x[i, beg-1:end] = 1
+            # Residue is disordered (1) if predicted so at least once
+            x = (np.sum(x, axis=0) > 0).astype(np.uint8)
+            # Update number of total residues
+            num_residues += m
+            # Update number of disordered residues
+            num_disorder += np.sum(x)
+
+        # Compute compositional bias
+        comp_bias = num_disorder / num_residues
+        # Return compositional bias
+        return comp_bias
+
+    # Compute multiple sequence alignment
+    def make_msa(self, sequences):
+
+        # Define temporary input file
+        fasta_file = tempfile.NamedTemporaryFile(delete=False)
+        # Fill input fasta file from sequences dict(acc: fasta entry)
+        with open(fasta_file.name, 'w') as ff:
+            # Loop through each sequence in input dictionary
+            for sequence_acc in sequences.keys():
+                # Write fasta entry
+                ff.write(sequences[sequence_acc] + '\n')
+
+
+
+        # Delete temporary inpput fasta file
+        os.remove(fasta_file)
+
+# Unit test
+if __name__ == '__main__':
+
+    # Define path to clusters file
+    CLUSTERS_PATH = '/nfs/production/xfam/pfam/jaina/MGnify_clusters/2019_05/clusters/mgy_seqs.cluster.tsv.gz'
+    # Define path to MGnify sequences
+    MGNIFY_PATH = '/nfs/production/xfam/pfam/data/mgnify/mgnify.fa.gz'
+
+    # Initialize new seed creation pipeline instance
+    seed = Seed(
+        clusters_path=CLUSTERS_PATH,
+        mgnify_path=MGNIFY_PATH
+    )
+
+    # Define input cluster name
+    cluster_name = 'MGYP000848664103'
+
+    # Set beginning time of the method
+    time_beg = time.time()
+    # Retrieve cluster members
+    cluster_members = seed.get_cluster_members(cluster_name)
+    # Set ending time of the method
+    time_end = time.time()
+    # Compute method duration
+    time_took = time_end - time_beg()
+    # Log
+    print('Took {:.02f} seconds to retrieve {:d} members of cluster {:s}'.format(
+        time_took,  # Method duration
+        len(cluster_members),  # Number of cluster sequences
+    ))
