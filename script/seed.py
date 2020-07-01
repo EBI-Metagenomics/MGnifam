13766bb70ffdd9b14b158b6199240836d0f8f966
Damiano Clementel <damiano.clementel@gmail.com>
2020-07-01 13:31:22 +0200

    Seed alignment pipeline refactoring

--- script/seed.py
+++ script/seed.py
@@ -18,6 +18,7 @@ from dask_jobqueue import LSFCluster
 import time
 import sys
 import os
+import re
 
 # Update python path for custom dependencies
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/..')
@@ -39,17 +40,18 @@ if __name__ == '__main__':
     # Initialize distributed cluster
     cluster = LSFCluster(
         cores=1,  # Number of cores per job
-        scheduler_port=39156,  # Port where scheduler will listen
+        # scheduler_port=39156,  # Port where scheduler will listen
         memory='2GB',  # Memory allocated per job
-        walltime='02:00',  # Time before shutting down worker jobs
-        log_directory=ROOT_PATH+'/tmp',  # Logs directory
+        walltime='04:00',  # Time before shutting down worker jobs
+        # log_directory=ROOT_PATH+'/tmp/log',  # Logs directory
         use_stdin=True  # Pass commands as stdin
     )
     # # Initialize local cluster
     # cluster = LocalCluster()
-    cluster.adapt(maximum_jobs=100)
+    cluster.adapt(minimum=20, maximum=100)
     # Debug
     print('Cluster:', cluster)
+    print('Dashboard is at', cluster.dashboard_link)
 
     # Initialize new client
     client = Client(cluster)
@@ -57,13 +59,41 @@ if __name__ == '__main__':
     print('Client:', client)
 
     # Instantiate new seed alignment pipeline
-    pipe = Seed(
+    pl = Seed(
         clusters_path=CLUSTERS_PATH,
         mgnify_path=MGNIFY_PATH,
         dask_client=client
     )
 
-    # Define a single cluster name
-    cluster_names = ['MGYP001051398202']
+    # # Define a single cluster name
+    # cluster_names = ['MGYP001051398202']
+    # # Define list of cluster names
+    # cluster_names = [
+    #     'MGYP000031769986', 'MGYP000002872901', 'MGYP000016484927',
+    #     'MGYP000029146431', 'MGYP000029539082', 'MGYP000036242905',
+    #     'MGYP000039062111', 'MGYP000040208386', 'MGYP000043559380',
+    #     'MGYP000046805896', 'MGYP000055620076', 'MGYP000068575450',
+    #     'MGYP000079685113', 'MGYP000089028905', 'MGYP000112885575',
+    #     'MGYP000116866465', 'MGYP000124177638', 'MGYP000128352270',
+    #     'MGYP000130617288'
+    # ]
+    # Initialize list of cluster names
+    cluster_names = list()
+    # Input file path
+    in_path = '/hps/nobackup2/production/metagenomics/dclementel/MGnifam_build/Batch_lists/build_list0300'
+    # Open input file
+    with open(in_path, 'r') as in_file:
+        # Read each file line
+        for line in in_file:
+            # Get cluster name
+            match = re.search(r'^(\S+)', line)
+            # Case line format does not match the expected one
+            if not match:
+                continue  # Skip iteration
+            # Otherwise, add cluster name to input list
+            cluster_names.append(match.group(1))
+
+    # Define output path
+    cluster_out_dir = ROOT_PATH + '/tmp'
     # Run the pipeline
-    pipe(cluster_names=cluster_names)
+    pl(cluster_names=cluster_names, cluster_dir=cluster_out_dir, verbose=True)
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,6 @@
 # Dependencies
 from dask.distributed import Client
+from tqdm import tqdm
 from glob import glob
 import time
 import json
@@ -9,6 +10,8 @@ import re
 
 # Custom dependencies
 import src.dataset as ds
+import src.disorder as disorder
+import src.msa as msa
 
 
 class Pipeline(object):
@@ -70,31 +73,120 @@ class Pipeline(object):
 class Seed(Pipeline):
 
     # Constructor
-    def __init__(self, clusters_path, mgnify_path, dask_client):
+    def __init__(
+        self, clusters_path, mgnify_path, dask_client,
+        mobidb_env=os.environ.copy(), mobidb_cmd=['mobidb_lite.py'],
+        muscle_env=os.environ.copy(), muscle_cmd=['muscle']
+    ):
         # Retrieve clusters dataset
         clusters_path = self.check_path(clusters_path)
         self.ds_clusters = ds.Cluster.from_list(clusters_path)
         # Retrieve MGnify dataset
         mgnify_path = self.check_path(mgnify_path)
         self.ds_mgnify = ds.MGnify.from_list(mgnify_path)
+        # Initialize a new MobiDB Lite instance
+        self.mobidb = disorder.MobiDBLite(cmd=mobidb_cmd, env=mobidb_env)
+        # Initialize a new Muscle instance
+        self.muscle = msa.Muscle(cmd=muscle_cmd, env=muscle_env)
         # Set client (Client(LocalCluster) by default)
         self.dask_client = dask_client
 
     # Run Seed alignment (for a single cluster)
-    def run(self, cluster_names):
+    def run(self, cluster_names, cluster_dir, verbose=False):
 
-        # Intialize timers
-        time_beg = time.time()
-        time_end = None
-        time_took = None
+        # # Get clusters output directory
+        # cluster_dir = os.path.dirname(cluster_dir)
+        # Case output directory does not exits
+        if not os.path.exists(cluster_dir):
+            # Make directory
+            os.mkdir(cluster_dir)
 
-        # Debug
-        print('Retrieving {:d} clusters:\n{}'.format(
-            len(cluster_names),  # Number of clusters
-            ', '.join(cluster_names)  # Verbose list of cluster names
-        ))
-        # Initialize dict(cluster name: list of sequence accession numbers)
+        # Retrieve cluster members from clusters .tsv dataset
+        cluster_members = self.get_cluster_members(
+            cluster_names=cluster_names,
+            verbose=verbose
+        )
+
+        # Check cluster members raises error
+        self.check_cluster_members(cluster_members)
+
+        # Define sequences accession numbers list
+        sequences_acc = [
+            # Get i-th sequence accession in cluster named n
+            cluster_members[n][i]
+            # For each cluster in cluster members
+            for n in cluster_members
+            # For each component of that cluster
+            for i in range(len(cluster_members[n]))
+        ]
+        # # Debug
+        # print(sequences_acc)
+
+        # Retrieve fasta sequences from MGnify .fa dataset
+        fasta_sequences = self.get_fasta_sequences(
+            sequences_acc=sequences_acc,
+            verbose=verbose
+        )
+
+        # Check for mismatching clusters wrt retrieved sequences
+        self.check_cluster_sequences(cluster_members, fasta_sequences)
+
+        # Compute compositional bias
+        comp_bias = self.compute_comp_bias(
+            cluster_members=cluster_members,
+            fasta_sequences=fasta_sequences,
+            verbose=verbose
+        )
+
+        # TODO Remove clusters with compositional bias too high
+
+        # Loop through each cluster name to make clusters directories
+        for cluster_name in cluster_members:
+            # Define cluster subdir
+            cluster_subdir = os.path.join(cluster_dir, cluster_name)
+            # Check if a subdirectory for current cluster exists
+            if not os.path.exists(cluster_subdir):
+                # Make sub directory
+                os.mkdir(cluster_subdir)
+
+        # Make seed alignment
+        self.make_sequences_aln(
+            cluster_members=cluster_members,
+            fasta_sequences=fasta_sequences,
+            cluster_dir=cluster_dir
+        )
+
+        # # Initialize cluster sequences dict(name: dict(seq acc: fasta entry))
+        # cluster_sequences = dict()
+        # # Fill cluster sequences dict
+        # for cluster_name in cluster_members:
+        #     # Add current cluster entry
+        #     cluster_sequences.setdefault(cluster_name, dict())
+        #     # Loop through each accession number in cluster members
+        #     for acc in cluster_members[cluster_name]:
+        #         # Add current fasta sequence to current cluster entry
+        #         cluster_sequences[cluster_name][acc] = fasta_sequences[acc]
+
+        # # Verbose log
+        # if verbose:
+        #     print('Retrieved sequences:')
+        #     print(json.dumps(cluster_sequences, indent=2))
+
+    # Retrieve cluster members for a batch of clusters
+    def get_cluster_members(self, cluster_names, verbose=False):
+        # Initialize output dict(cluster name: sequence acc)
         cluster_members = dict()
+        # Intialize timers
+        time_beg, time_end = time.time(), None
+
+        # Verbose log
+        if verbose:
+            # Get a summary of cluster names
+            print('Retrieving {:d} clusters:\n{}'.format(
+                len(cluster_names),  # Number of clusters
+                ', '.join(cluster_names)  # Verbose list of cluster names
+            ))
+
         # Submit jobs for searching cluster members
         futures = self.dask_client.map(
             # Search for cluster members in current chunk
@@ -115,106 +207,217 @@ class Seed(Pipeline):
 
         # Update timers
         time_end = time.time()
-        time_took = time_end - time_beg
-        # Debug
-        print('Took {:.0f} seconds to search members of {:d} clusters'.format(
-            time_took,  # Time required to distributed search
-            len(cluster_members.keys())  # Number of clusters
-        ))
+        # Verbose log
+        if verbose:
+            # Get execution time summary
+            print('Took {:.0f} seconds to search members of {:d} clusters'.format(
+                time_end - time_beg,  # Time required to distributed search
+                len(cluster_members.keys())  # Number of clusters
+            ))
 
-        # # Debug
-        # print(json.dumps(cluster_members))
+        # Return cluster members dict
+        return cluster_members
 
-        # Reset timer
-        time_beg = time.time()
-
-        # Get list of clusters with no members
+    # Check that all clusters have at least one sequence accession
+    def check_cluster_members(self, cluster_members):
+        # Check for clusters with no members (eventually raise error)
         empty_clusters = [n for n, m in cluster_members.items() if not len(m)]
         # Check if at least one cluster has no member
         if empty_clusters:
-            # Define error text
-            error_text = 'No sequences found for {:d} clusters: {:s}'.format(
+            # Raise new error
+            raise KeyError('No sequences found for {:d} clusters: {:s}'.format(
                 len(empty_clusters),  # Number of clusters with no members
                 ', '.join(empty_clusters)  # Verbose string of empty clusters
-            )
-            # Raise new error
-            raise KeyError(error_text)
-        # Free unused memory
-        del empty_clusters
+            ))
 
-        # Initialize dict(sequence accession: fasta entry)
-        cluster_sequences = dict()
+    # Retrieve cluster sequences for given sequences accession numbers
+    def get_fasta_sequences(self, sequences_acc, verbose=False):
+        # Initialize output dict(sequence acc: fasta entry)
+        fasta_sequences = dict()
+        # Initialize timers
+        time_beg, time_end = time.time(), None
+
         # Submit jobs for searching cluster members sequences
         futures = self.dask_client.map(
             # Search for cluster members in current chunk
-            lambda ds: ds.search([
-                # Get i-th sequence accession in cluster named n
-                cluster_members[n][i]
-                for n in cluster_members.keys()
-                for i in range(len(cluster_members[n]))
-            ]),
+            lambda ds: ds.search(sequences_acc),
             # List of cluster datasets
             self.ds_mgnify
         )
         # Retrieve found sequences (list of dict)
         results = self.dask_client.gather(futures)
-        # # Debug
-        # print('Debug', json.dumps(results))
         # Loop through resulting sequences dictionaries
         for i in range(len(results)):
             # Loop through retrieved sequence accession numbers
-            for sequence_acc in results[i]:
+            for acc in results[i]:
                 # Update cluster sequences dictionary
-                cluster_sequences[sequence_acc] = results[i][sequence_acc]
+                fasta_sequences[acc] = results[i][acc]
 
         # Update timers
         time_end = time.time()
-        time_took = time_end - time_beg
-        # Debug
-        print('Took {:.0f} seconds to search for {:d} sequences:'.format(
-            time_took,  # Time required to distributed search
-            len(cluster_sequences)  # Number of sequences
-        ))
-        # print(json.dumps(cluster_sequences))
+        # Verbose log
+        if verbose:
+            # Log time taken for fasta sequences distributed search
+            print('Took {:.0f} seconds to search for {:d} sequences'.format(
+                time_end - time_beg,  # Time required to distributed search
+                len(fasta_sequences)  # Number of sequences
+            ))
 
+        # Return fasta sequences
+        return fasta_sequences
+
+    # Check that all the sequences have been retrieved
+    def check_cluster_sequences(self, cluster_members, fasta_sequences):
         # Initialize list of clusters with mismatching sequences
         mismatch_clusters = list()
+        # Get sequence accession numbers in retrieved fasta sequences
+        sequences_acc = set(fasta_sequences.keys())
         # Fill mismatching clusters
         for cluster_name in cluster_members.keys():
-            # Get sequence accession numbers for current cluster
-            sequences_acc = set(cluster_members[cluster_name])
+            # Define current cluster's seuqence accessionnumbers
+            cluster_acc = set(cluster_members[cluster_name])
             # Case some sequence has not been retrieved
-            if sequences_acc - set(cluster_sequences.keys()):
+            if cluster_acc - sequences_acc:
                 # Add cluster name to list of mismatching ones
                 mismatch_clusters.append(cluster_name)
         # Case there is at least one mismatching cluster
         if mismatch_clusters:
-            # Define new error message
-            error_text = 'Could not retrieve all sequences for {:d} clusters: {}'.format(
-                len(mismatch_clusters),  # Number of mismatching clusters
-                ', '.join(mismatch_clusters)  # Mismatching clusters
-            )
             # Raise new error
-            raise KeyError(error_text)
-        # Free unused memory
-        del mismatch_clusters
-
-        # Debug
-        print('Retrieved sequences:')
-        print(json.dumps(cluster_sequences, indent=2))
-
-    # Retrieve cluster members for a batch of clusters
-    def get_cluster_members(self, cluster_names):
-        raise NotImplementedError
-
-    # Retrieve cluster sequences for given sequences accession numbers
-    def get_fasta_sequences(self, sequences_acc):
-        raise NotImplementedError
+            raise KeyError(
+                'Could not retrieve all sequences for {:d} clusters: {}'.format(
+                    len(mismatch_clusters),  # Number of mismatching clusters
+                    ', '.join(mismatch_clusters)  # Mismatching clusters
+                )
+            )
 
     # Compute compositional bias for every sequence
-    def compute_comp_bias(self, fasta_sequences):
-        raise NotImplementedError
+    def compute_comp_bias(self, cluster_members, fasta_sequences, verbose=False):
+
+        # Initialize timers
+        time_beg, time_end = time.time(), None
+
+        # Initialize disorder dict(sequence acc: list of boolean lists)
+        disorder_sequences = dict()
+        # Get reference to MobiDB Lite instance
+        mobidb = self.mobidb
+        # Verbose log
+        if verbose:
+            print('Computing disordered regions for {:d} sequences...'.format(
+                len(fasta_sequences)
+            ))
+        # Run predictior on each  entry in fasta sequence dict(acc: fasta enry)
+        futures = self.dask_client.map(
+            # Function make a prediction over a single sequence
+            lambda acc: mobidb.run(
+                sequences={acc: fasta_sequences[acc]},
+                verbose=False
+            ),
+            # Map function over each accession number in fasta sequences
+            [*fasta_sequences.keys()]
+        )
+        # Get distributed computing results
+        results = self.dask_client.gather(futures)
+        # Loop through every result
+        for i in range(len(results)):
+            # Get i-th result
+            result = results[i]
+            # Loop through each sequence accession in i-th result
+            for acc in result.keys():
+                # Eventually make new entry in disorder dictionart
+                disorder_sequences.setdefault(acc, [])
+                # Append disorder predictions
+                disorder_sequences[acc] += result[acc]
+
+        # Update timers
+        time_end = time.time()
+        # Verbose log
+        if verbose:
+            # Get execution time summary
+            print('Took {:.0f} seconds to compute disordered regions for {:d} fasta sequences'.format(
+                time_end - time_beg,  # Time required to distributed search
+                len(disorder_sequences)  # Number of fasta sequences
+            ))
+
+        # Initialize compossitional bias dict(cluster name: comp bias)
+        comp_bias = dict()
+        # Verbose log
+        if verbose:
+            print('Computing compositional bias for {:d} sequences...'.format(
+                len(disorder_sequences)
+            ))
+        # For each cluster, compute compute compositional bias
+        for cluster_name in tqdm(cluster_members, disable=(not verbose)):
+            # Initialize current cluster disorder predictions
+            cluster_disorder = dict()
+            # Get disordered regions for current cluster sequences
+            for acc in cluster_members[cluster_name]:
+                # Store disordered regions for current cluster sequences
+                cluster_disorder[acc] = disorder_sequences[acc]
+            # Apply a threshold over disorder predictions
+            cluster_disorder = disorder.compute_threshold(
+                sequences=cluster_disorder,
+                threshold=1,
+                inclusive=True
+            )
+            # Compute compositional bias
+            comp_bias[cluster_name] = disorder.compute_comp_bias(
+                sequences=cluster_disorder
+            )
+
+        # Return dict(cluster name: compositional bias)
+        return comp_bias
 
     # Make multiple sequence alignment
-    def align_sequences(self, fasta_sequences):
-        raise NotImplementedError
+    def make_sequences_aln(self, cluster_members, fasta_sequences, cluster_dir, verbose=False):
+
+        # # Initialize dict(cluster name: dict(sequence acc: fasta entry))
+        # cluster_sequences = dict()
+        # # Fill cluster sequences dict
+        # for cluster_name in cluster_members:
+        #     # Add current cluster entry
+        #     cluster_sequences.setdefault(cluster_name, list())
+        #     # Loop through each accession number in cluster members
+        #     for acc in cluster_members[cluster_name]:
+        #         # Add current fasta sequence to current cluster entry
+        #         cluster_sequences[cluster_name] += [fasta_sequences[acc]]
+
+        # Initialize timers
+        time_beg, time_end = time.time(), None
+        # Verbose log
+        if verbose:
+            print('Making multiple sequence alignments for {:d} clusters...'.format(
+                len(cluster_members)
+            ))
+
+        # Get reference to muslce instance
+        muscle = self.muscle
+        # Feed cluster sequences to alignment algorithm
+        futures = self.dask_client.map(
+            # Given cluster name, align its inner sequences
+            lambda cluster_name: muscle.run(
+                sequences=[
+                    # Get list of all sequences in current cluster
+                    fasta_sequences[acc]
+                    # Loop through all current cluster members
+                    for acc in cluster_members[cluster_name]
+                ],
+                acc_regex='>(\S+)',
+                verbose=False
+            ).to_aln(
+                # Define where to store current multiple sequence alignment
+                out_path=os.path.join(cluster_dir, cluster_name, 'SEED')
+            ),
+            # Map function over each cluster name
+            [*cluster_members.keys()]
+        )
+        # Wait for all alignments to finish
+        self.dask_client.gather(futures)
+
+        # Update timers
+        time_end = time.time()
+        # Verbose log
+        if verbose:
+            print('Took {:.0f} seconds to make {:d} multiple sequence alignments'.format(
+                time_end - time_beg,
+                len(cluster_members)
+            ))
